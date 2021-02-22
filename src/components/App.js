6a327bc3ccec9f5963bfb48c2155e934f2c8f72a
Mukola Dera <dns3211@gmail.com>
2021-02-22 16:51:22 +0200

    hw-01-fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,35 @@
 import React from 'react';
 
 import Profile from './profile/Profile';
-import userData from "../user.json";
+import userData from '../data/user.json';
 
 import Statistics from './statistics/Statistics';
-import statisticsData from '../statistical-data.json';
+import statisticsData from '../data/statistical-data.json';
 
 import FriendList from './friendList/FriendList';
-import friendsData from '../friends.json';
+import friendsData from '../data/friends.json';
 
-import TransactionHistory from "./transactionHistory/TransactionHistory";
-import transactionData from "../transactions.json";
+import TransactionHistory from './transactionHistory/TransactionHistory';
+import transactionData from '../data/transactions.json';
 
 const App = () => {
-    return (
-      <>
-        <Profile
-         url={userData.avatar}
-         name={userData.name}
-         tag={userData.tag}
-         location={userData.location}
-         stats={userData.stats}/>
+  return (
+    <>
+      <Profile
+        url={userData.avatar}
+        name={userData.name}
+        tag={userData.tag}
+        location={userData.location}
+        stats={userData.stats}
+      />
 
-        <Statistics
-          title={'Upload stats'}
-          stats={statisticsData} 
-         />;
-        
-        <FriendList
-          friends = {friendsData}
-        />
+      <Statistics title={'Upload stats'} stats={statisticsData} />
 
-        <TransactionHistory 
-          transaction = {transactionData}
-        />
-      </>    
-    );
+      <FriendList friends={friendsData} />
+
+      <TransactionHistory transaction={transactionData} />
+    </>
+  );
 };
 
 export default App;
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,45 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import style from './Profile.module.css'
+import style from './Profile.module.css';
 
+const Profile = ({ url, name, tag, location, stats }) => (
+  <div className={style.profile}>
+    <div className={style.description}>
+      <img src={url} alt="Аватар пользователя" className={style.avatar} />
+      <p className={style.name}>{name}</p>
+      <p className={style.tag}>{tag}</p>
+      <p className={style.location}>{location}</p>
+    </div>
 
-const Profile = ({url, name, tag, location, stats}) => (
-        <div className={style.profile}>
-          <div className={style.description}>
-                <img
-                    src={url}
-                    alt="Аватар пользователя"
-                    className={style.avatar}
-                />
-                <p className={style.name}>{name}</p>
-                <p className={style.tag}>{tag}</p>
-                <p className={style.location}>{location}</p>
-          </div>
-
-            <ul className={style.stats}>
-                <li className={style.item}>
-                    <span className={style.label}>Followers </span>
-                    <span className={style.quantity}>{stats.followers}</span>
-                </li>
-                <li className={style.item}>
-                    <span className={style.label}>Views </span>
-                    <span className={style.quantity}>{stats.views}</span>
-                </li>
-                <li className={style.item}>
-                    <span className={style.label}>Likes </span>
-                    <span className={style.quantity}>{stats.likes}</span>
-                </li>
-            </ul>
-        </div>
+    <ul className={style.stats}>
+      <li className={style.item}>
+        <span className={style.label}>Followers </span>
+        <span className={style.quantity}>{stats.followers}</span>
+      </li>
+      <li className={style.item}>
+        <span className={style.label}>Views </span>
+        <span className={style.quantity}>{stats.views}</span>
+      </li>
+      <li className={style.item}>
+        <span className={style.label}>Likes </span>
+        <span className={style.quantity}>{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
 );
 
 Profile.propTypes = {
-    url: PropTypes.string,
-    name: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    stats: PropTypes.objectOf(PropTypes.number).isRequired,
-
+  url: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  stats: PropTypes.objectOf(PropTypes.number).isRequired,
 };
 
 export default Profile;
